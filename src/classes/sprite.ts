dfaef964660c3684a4d018ea5575d8db71b6e0f6
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-01-13 03:56:28 -0600

    make a couple args mandatory & add default settings

--- src/classes/sprite.ts
+++ src/classes/sprite.ts
@@ -1,140 +1,140 @@
 import { Tile } from "./tile";
-import { SpriteClassOptions, SpriteCoordinates, SpriteData, TileOptions, Tile as TileTypes } from "../../types/sprite.types";
+import { SpriteClassOptions, SpriteCoordinates, SpriteData, TileOptions } from "../../types/sprite.types";
 
 import sharp from "sharp";
 
+const DEFAULT_OPTIONS: SpriteClassOptions = {
+  gap: 1,
+};
+
 function booleanToGap(gap?: boolean | number): number {
-   if (typeof gap === "number") return gap;
-   if (typeof gap === "boolean") return !gap ? 0 : 1;
-   return 1;
+  return typeof gap === "number" ? gap : gap === true ? 1 : 0;
 }
 
 export default class Sprite {
+  tileHeight: number;
+  tileWidth: number;
+  gap: number;
 
-   tileHeight: number;
-   tileWidth: number;
-   gap: number = 0;
+  tiles: Tile[] = [];
 
-   tiles: Tile[] = [];
+  constructor(tileWidth: number, tileHeight: number, options?: SpriteClassOptions) {
+    this.tileHeight = tileWidth;
+    this.tileWidth = tileHeight;
 
+    options = { ...DEFAULT_OPTIONS, ...options };
 
-   constructor(tileWidth: number, tileHeight: number, options?: SpriteClassOptions) {
-      this.tileHeight = tileWidth
-      this.tileWidth = tileHeight;
+    this.gap = booleanToGap(options.gap);
+  }
 
-      this.gap = booleanToGap(options?.gap);
-   }
+  setTileHeight(height: number) {
+    this.tileHeight = height;
+  }
 
+  setTileWidth(width: number) {
+    this.tileWidth = width;
+  }
 
-   setTileHeight(height: number) {
-      this.tileHeight = height;
-   }
+  addTile(options: Tile | TileOptions) {
+    let tile;
+    if (options instanceof Tile) {
+      tile = options;
+    } else {
+      tile = new Tile(options);
+    }
+    this.tiles.push(tile);
+    return tile;
+  }
 
+  removeTile(tile: Tile): Boolean {
+    const index = this.tiles.indexOf(tile);
+    if (index !== -1) {
+      this.tiles.splice(index, 1);
+      return true;
+    }
 
-   setTileWidth(width: number) {
-      this.tileWidth = width;
-   }
+    return false;
+  }
 
+  json(includeImages = false): SpriteData {
+    const cols = Math.ceil(Math.sqrt(this.tiles.length));
+    const rows = cols * (cols - 1) >= this.tiles.length ? cols - 1 : cols;
 
-   addTile(options: Tile | TileOptions) {
-      let tile;
-      if (options instanceof Tile) {
-         tile = options;
-      } else {
-         tile = new Tile(options);
+    const width = this.tileWidth ?? 0;
+    const height = this.tileHeight ?? 0;
+
+    const totalWidth = width * cols + this.gap * (cols - 1);
+    const totalHeight = height * rows + this.gap * (rows - 1);
+
+    const sprites: Record<string, SpriteCoordinates> = {};
+    this.tiles.forEach((tile, i) => {
+      if (!tile.isEnabled()) return;
+
+      const id = tile.getId();
+      if (sprites.hasOwnProperty(id)) {
+        console.warn("Duplicate tile id. Skipping over it");
+        return;
       }
-      this.tiles.push(tile);
-      return tile;
-   }
 
-   removeTile(tile: Tile): Boolean {
-      let index = this.tiles.indexOf(tile);
-      if (index !== -1) {
-         this.tiles.splice(index, 1);
-         return true;
+      sprites[id] = {
+        x: (i % cols) * width + this.gap * (i % cols),
+        y: Math.floor(i / cols) * height + this.gap * Math.floor(i / cols),
+      };
+
+      if (includeImages) {
+        sprites[id].image = tile.getImage();
       }
+    });
 
-      return false;
-   }
+    return {
+      tileWidth: this.tileWidth,
+      tileHeight: this.tileHeight,
+      totalWidth: totalWidth,
+      totalHeight: totalHeight,
+      sprites: sprites,
+    };
+  }
 
-   json(includeImages = false): SpriteData {
-      const cols = Math.ceil(Math.sqrt(this.tiles.length));
-      const rows = (cols * (cols - 1) >= this.tiles.length) ? (cols - 1) : cols;
+  async generate() {
+    const spriteConfig = this.json(true);
 
-      const width = (this.tileWidth ?? 0);
-      const height = (this.tileHeight ?? 0);
+    const sprite = sharp({
+      create: {
+        width: spriteConfig.totalWidth,
+        height: spriteConfig.totalHeight,
+        channels: 4,
+        background: {
+          r: 0,
+          g: 0,
+          b: 0,
+          alpha: 0,
+        },
+      },
+    });
 
-      const totalWidth = width * cols + (this.gap * (cols - 1));
-      const totalHeight = height * rows + (this.gap * (rows - 1));
+    const sprites = Object.values(spriteConfig.sprites);
 
-      const sprites: Record<string, SpriteCoordinates> = {};
-      this.tiles.forEach((tile, i) => {
+    let inputs = [];
 
-         if (!tile.isEnabled()) return;
+    for (let i = 0; i < sprites.length; i++) {
+      const tile = sprites[i];
+      if (!tile.image) continue;
 
-         const id = tile.getId();
-         if (sprites.hasOwnProperty(id)) {
-            console.warn("Duplicate tile id. Skipping over it")
-            return;
-         }
+      inputs.push({
+        input: await sharp(tile.image)
+          .resize({
+            width: spriteConfig.tileWidth,
+            height: spriteConfig.tileHeight,
+            fit: sharp.fit.fill,
+          })
+          .png()
+          .toBuffer(),
+        left: tile.x,
+        top: tile.y,
+      });
+    }
+    await sprite.composite(inputs);
 
-         sprites[id] = {
-            x: (i % cols) * width + (this.gap * (i % cols)),
-            y: Math.floor((i) / cols) * height + (this.gap * Math.floor((i) / cols))
-         };
-
-         if (includeImages) {
-            sprites[id].image = tile.getImage();
-         }
-      })
-
-      return {
-         tileWidth: this.tileWidth,
-         tileHeight: this.tileHeight,
-         totalWidth: totalWidth,
-         totalHeight: totalHeight,
-         sprites: sprites
-      }
-   }
-
-
-   async generate() {
-      const spriteConfig = this.json(true);
-
-      const sprite = sharp({
-         create: {
-            width: spriteConfig.totalWidth,
-            height: spriteConfig.totalHeight,
-            channels: 4,
-            background: {
-               r: 0,
-               g: 0,
-               b: 0,
-               alpha: 0
-            }
-         }
-      })
-
-      const sprites = Object.values(spriteConfig.sprites);
-
-      let inputs = [];
-
-      for (let i = 0; i < sprites.length; i++) {
-         const tile = sprites[i];
-         if (!tile.image) continue;
-
-         inputs.push({
-            input: await sharp(tile.image).resize({
-               width: spriteConfig.tileWidth,
-               height: spriteConfig.tileHeight,
-               fit: sharp.fit.fill
-            }).png().toBuffer(),
-            left: tile.x,
-            top: tile.y
-         });
-      }
-      await sprite.composite(inputs)
-
-      sprite.toFile("output.webp");
-   }
+    sprite.toFile("output.webp");
+  }
 }
--- src/classes/tile.ts
+++ src/classes/tile.ts
@@ -2,39 +2,39 @@ import { TileOptions } from "../../types/sprite.types";
 import crypto, { BinaryLike } from "crypto";
 
 export class Tile {
+  id?: string;
+  image?: Buffer;
+  enabled = true;
 
-   id?: string;
-   image?: Buffer;
-   enabled = true;
+  constructor(options: TileOptions) {
+    this.id = options.id;
+    this.image = options.image;
+    this.enabled = options.enabled ?? this.enabled;
+  }
 
+  getId(): string {
+    if (!this.id && !this.image) {
+      throw new Error("Tile needs image");
+    }
 
-   constructor(options?: TileOptions) {
-      this.id = options?.id;
-      this.image = options?.image;
-      this.enabled = options?.enabled ?? this.enabled;
-   }
+    return (
+      this.id ??
+      crypto
+        .createHash("md5")
+        .update(this.image as BinaryLike)
+        .digest("hex")
+    );
+  }
 
+  isEnabled(): Boolean {
+    return this.enabled;
+  }
 
-   getId(): string {
-      if (!this.id && !this.image) {
-         throw new Error("Tile needs image")
-      }
+  getImage(): Buffer {
+    if (!this.image) {
+      throw new Error("Tile needs image");
+    }
 
-      return this.id ?? crypto.createHash("md5").update(this.image as BinaryLike).digest("hex");
-   }
-
-
-   isEnabled(): Boolean {
-      return this.enabled;
-   }
-
-
-   getImage(): Buffer {
-      if (!this.image) {
-         throw new Error("Tile needs image")
-      }
-
-      return this.image;
-   }
-
+    return this.image;
+  }
 }
